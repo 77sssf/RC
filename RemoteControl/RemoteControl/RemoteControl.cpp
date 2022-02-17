da62cd0a8f142e7c9646440c14eb1e3278d41f2c
77sssf <moebyhs@gmail.com>
2022-02-17 15:06:48 +0800

    Space to Tabs

--- RemoteControl/RemoteControl/RemoteControl.cpp
+++ RemoteControl/RemoteControl/RemoteControl.cpp
@@ -12,15 +12,15 @@
 CWinApp theApp;
 
 void SetStarupDir() {
-    //  C:\Users\moeby\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
-    CString sPath = TEXT("C:\\Users\\moeby\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RemoteControl.exe");
-    CString sCmd = GetCommandLine();
-    sCmd.Replace("\"", "");
-    BOOL ret = CopyFile(sCmd, sPath, FALSE);
-    if (ret == FALSE) {
+	//  C:\Users\moeby\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+	CString sPath = TEXT("C:\\Users\\moeby\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RemoteControl.exe");
+	CString sCmd = GetCommandLine();
+	sCmd.Replace("\"", "");
+	BOOL ret = CopyFile(sCmd, sPath, FALSE);
+	if (ret == FALSE) {
 		MessageBox(NULL, TEXT("设置开启自动启动失败, 请检查权限, 程序启动失败"), TEXT("错误"), MB_OK | MB_ICONERROR | MB_TOPMOST);
 		exit(0);
-    }
+	}
 }
 
 void SetReg() {
@@ -51,127 +51,118 @@ void SetReg() {
 }
 
 void AutoInvoke() {
-    //  C:\\Windows\\System32\\
-    //  HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run
-    if (PathFileExists("C:\\Windows\\SysWOW64\\RemoteControl.exe") || PathFileExists("C:\\Users\\moeby\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RemoteControl.exe")) {
-        return;
-    }
-    CString programInfo = TEXT("该程序只允许用于合法用途\n");
-    programInfo += TEXT("继续运行该程序将使系统处于被监控状态\n");
+	//  C:\\Windows\\System32\\
+	//  HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run
+	if (PathFileExists("C:\\Windows\\SysWOW64\\RemoteControl.exe") || PathFileExists("C:\\Users\\moeby\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\RemoteControl.exe")) {
+		return;
+	}
+	CString programInfo = TEXT("该程序只允许用于合法用途\n");
+	programInfo += TEXT("继续运行该程序将使系统处于被监控状态\n");
 	programInfo += TEXT("如果不希望被监控, 请按\"取消\", 退出程序\n");
-    programInfo += TEXT("点击\"是\", 该程序启动并且设置开机自动启动\n");
+	programInfo += TEXT("点击\"是\", 该程序启动并且设置开机自动启动\n");
 	programInfo += TEXT("点击\"否\", 该程序启动不会设置开启自动启动\n");
-    int ret = MessageBox(NULL, programInfo, TEXT("警告"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
-    if (ret == IDYES) {
-        SetStarupDir();
-    }
-    else if(ret == IDCANCEL) {
-        exit(0);
-    }
-    else {
-        //  TODO : Nothing;
-    }
+	int ret = MessageBox(NULL, programInfo, TEXT("警告"), MB_YESNOCANCEL | MB_ICONWARNING | MB_TOPMOST);
+	if (ret == IDYES) {
+		SetStarupDir();
+	}
+	else if(ret == IDCANCEL) {
+		exit(0);
+	}
+	else {
+		//  TODO : Nothing;
+	}
 }
 
 void ShowError() {
-    LPSTR lpMsgBuf = NULL;
-    FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
+	LPSTR lpMsgBuf = NULL;
+	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER,
 		NULL,
 		GetLastError(),
 		MAKELANGID(LANG_NEUTRAL, LANG_CHINESE_SIMPLIFIED),
 		(LPSTR)&lpMsgBuf, 
-        0, NULL);
-    OutputDebugString(lpMsgBuf);
-    LocalFree(lpMsgBuf);
+		0, NULL);
+	OutputDebugString(lpMsgBuf);
+	LocalFree(lpMsgBuf);
 }
 
 BOOL CheckAdmin() {
-    HANDLE hToken = NULL;
-    BOOL ret = OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken);
-    if (ret == FALSE) {
-        ShowError();
-        return FALSE;
-    }
-    TOKEN_ELEVATION eve;
-    DWORD len = 0;
-    ret = GetTokenInformation(hToken, TokenElevation, &eve, sizeof(eve), &len);
-    if (ret == FALSE) {
-        
-    }
-    CloseHandle(hToken);
-    if (len == sizeof(eve)) {
-        return eve.TokenIsElevated;
-    }
-    return FALSE;
+	HANDLE hToken = NULL;
+	BOOL ret = OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken);
+	if (ret == FALSE) {
+		ShowError();
+		return FALSE;
+	}
+	TOKEN_ELEVATION eve;
+	DWORD len = 0;
+	ret = GetTokenInformation(hToken, TokenElevation, &eve, sizeof(eve), &len);
+	if (ret == FALSE) {
+		
+	}
+	CloseHandle(hToken);
+	if (len == sizeof(eve)) {
+		return eve.TokenIsElevated;
+	}
+	return FALSE;
 }
 
 void RunAsAdmin() {
 	//  1. 获取管理员权限
-    //  2. 使用管理员权限创建进程
-    HANDLE hToken = NULL;
-    BOOL ret = LogonUser("Administrator", NULL, NULL, LOGON32_LOGON_INTERACTIVE, LOGON32_PROVIDER_DEFAULT, &hToken);
-    if (ret == FALSE) {
-        ShowError();
-        CloseHandle(hToken);
-        exit(0);
-    }
-    STARTUPINFOW si = {};
-    PROCESS_INFORMATION pi = {};
-    LPWSTR sPath = NULL;
-    sPath = GetCommandLineW();
-    
-    //ret = CreateProcessWithTokenW(hToken, LOGON_WITH_PROFILE,NULL, sPath, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);
-    ret = CreateProcessWithLogonW(L"Administrator", NULL, NULL, LOGON_WITH_PROFILE, NULL, sPath, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);
-    if (ret == FALSE) {
-        ShowError();
-		CloseHandle(hToken);
-        exit(0);
-    }
-    WaitForSingleObject(pi.hProcess, INFINITE);
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
-    CloseHandle(hToken);
+	//  2. 使用管理员权限创建进程
+	STARTUPINFOW si = {};
+	PROCESS_INFORMATION pi = {};
+	LPWSTR sPath = NULL;
+	sPath = GetCommandLineW();
+	
+	//ret = CreateProcessWithTokenW(hToken, LOGON_WITH_PROFILE,NULL, sPath, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);
+	BOOL ret = CreateProcessWithLogonW(L"Administrator", NULL, NULL, LOGON_WITH_PROFILE, NULL, sPath, CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi);
+	if (ret == FALSE) {
+		ShowError();
+		exit(0);
+	}
+	WaitForSingleObject(pi.hProcess, INFINITE);
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
 }
 
 int main()
 {
-    if (CheckAdmin()) {
-        MessageBox(NULL, TEXT("当前为管理员"), TEXT("账户身份"), MB_OK);
-    }
-    else {
+	if (CheckAdmin()) {
+		MessageBox(NULL, TEXT("当前为管理员"), TEXT("账户身份"), MB_OK);
+	}
+	else {
 		//  TODO : 提升权限
 		MessageBox(NULL, TEXT("当前为普通用户"), TEXT("账户身份"), MB_OK);
 		RunAsAdmin();
 		return 0;
-    }
+	}
 
-    int nRetCode = 0;
-    HMODULE hModule = ::GetModuleHandle(nullptr);
+	int nRetCode = 0;
+	HMODULE hModule = ::GetModuleHandle(nullptr);
 
-    if (hModule != nullptr)
-    {
-        // initialize MFC and print and error on failure
-        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
-        {
-            // TODO: code your application's behavior here.
-            wprintf(L"Fatal Error: MFC initialization failed\n");
-            nRetCode = 1;
-        }
-        else
-        {	
-            //AutoInvoke();
-            CCommandHanle cmd;
-            if (!CSrvSocket::getInstance()->Run(&CCommandHanle::RunCommand, &cmd)) {
-                MessageBox(NULL, TEXT("网络初始化异常, 请检查网络环境"), TEXT("网络错误"), MB_OK | MB_ICONERROR);
-                exit(-1);
-            }
-        }
-    }
-    else
-    {
-        // TODO: change error code to suit your needs
-        wprintf(L"Fatal Error: GetModuleHandle failed\n");
-        nRetCode = 1;
-    }
-    return nRetCode;
+	if (hModule != nullptr)
+	{
+		// initialize MFC and print and error on failure
+		if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+		{
+			// TODO: code your application's behavior here.
+			wprintf(L"Fatal Error: MFC initialization failed\n");
+			nRetCode = 1;
+		}
+		else
+		{	
+			//AutoInvoke();
+			CCommandHanle cmd;
+			if (!CSrvSocket::getInstance()->Run(&CCommandHanle::RunCommand, &cmd)) {
+				MessageBox(NULL, TEXT("网络初始化异常, 请检查网络环境"), TEXT("网络错误"), MB_OK | MB_ICONERROR);
+				exit(-1);
+			}
+		}
+	}
+	else
+	{
+		// TODO: change error code to suit your needs
+		wprintf(L"Fatal Error: GetModuleHandle failed\n");
+		nRetCode = 1;
+	}
+	return nRetCode;
 }
